7e5d34935f5b82d050af002200da7b12bc643b22
JebCodeOficial <jebcode@outlook.com.br>
2024-07-05 11:03:56 -0300

    Desafio parte-2

--- src/pages/Categories/index.tsx
+++ src/pages/Categories/index.tsx
@@ -1,106 +1,48 @@
+import { useEffect, useState } from 'react'
 import ProductsList from '../../components/ProductsList'
-import Game from '../../models/Game'
+import { Game } from '../Home'
 
-import resident from '../../assets/img/resident.png'
-import fifa from '../../assets/img/fifa.png'
-import diablo4 from '../../assets/img/diablo.png'
-import starWars from '../../assets/img/star_wars.png'
-import streetFighter from '../../assets/img/street-fighter.png'
-import zelda from '../../assets/img/zelda.png'
+const Categories = () => {
+  const [gamesAcao, setGamesAcao] = useState<Game[]>([])
+  const [gamesEsportes, setGamesEsportes] = useState<Game[]>([])
+  const [gamesSimulacao, setGamesSimulacao] = useState<Game[]>([])
+  const [gamesLuta, setGamesLuta] = useState<Game[]>([])
+  const [gamesRPG, setGamesRPG] = useState<Game[]>([])
 
-const promocoes: Game[] = [
-  {
-    id: 1,
-    category: 'Ação',
-    description:
-      'Resident Evil 4, conhecido no Japão como Biohazard 4, é um jogo eletrônico de survival horror...',
-    image: resident,
-    infos: ['10%', 'R$ 199,90'],
-    system: 'Windows',
-    title: 'Resident Evil 4 - Remake'
-  },
-  {
-    id: 2,
-    category: 'Ação',
-    description:
-      'Resident Evil 4, conhecido no Japão como Biohazard 4, é um jogo eletrônico de survival horror...',
-    image: resident,
-    infos: ['10%', 'R$ 199,90'],
-    system: 'Windows',
-    title: 'Resident Evil 4 - Remake'
-  },
-  {
-    id: 3,
-    category: 'Esporte',
-    description:
-      'EA SPORTS™ FIFA 23 traz o Jogo de Todo Mundo aos gramados com a tecnologia HyperMotion2...',
-    image: fifa,
-    infos: ['-50%', 'R$ 99,90'],
-    system: 'PS5',
-    title: 'FIFA 23'
-  },
-  {
-    id: 4,
-    category: 'Esporte',
-    description:
-      'EA SPORTS™ FIFA 23 traz o Jogo de Todo Mundo aos gramados com a tecnologia HyperMotion2...',
-    image: fifa,
-    infos: ['-50%', 'R$ 99,90'],
-    system: 'PS5',
-    title: 'FIFA 23'
-  }
-]
+  useEffect(() => {
+    fetch('https://fake-api-tau.vercel.app/api/eplay/acao')
+      .then((res) => res.json())
+      .then((res) => setGamesAcao(res))
 
-const emBreve: Game[] = [
-  {
-    id: 5,
-    category: 'RPG',
-    description:
-      'Diablo IV é um RPG de ação em desenvolvimento pela Blizzard Entertainment.',
-    image: diablo4,
-    infos: ['05/04'],
-    system: 'Windows',
-    title: 'Diablo 4'
-  },
-  {
-    id: 6,
-    category: 'Aventura',
-    description:
-      'Star Wars Jedi: Survivor é um próximo jogo de ação e aventura desenvolvido pela Respawn...',
-    image: starWars,
-    infos: ['05/04'],
-    system: 'Windows',
-    title: 'Star Wars Jedi Survivor'
-  },
-  {
-    id: 7,
-    category: 'Esporte',
-    description:
-      'Street Fighter 6 é um próximo jogo de luta desenvolvido e publicado pela Capcom.',
-    image: streetFighter,
-    infos: ['05/04'],
-    system: 'Windows',
-    title: 'Street Fighter 6'
-  },
-  {
-    id: 8,
-    category: 'RPG',
-    description:
-      'Uma aventura épica pela terra e pelos céus de Hyrule aguarda em The Legend of Zelda™...',
-    image: zelda,
-    infos: ['05/04'],
-    system: 'Switch',
-    title: 'The Legend of Zelda - TOK'
-  }
-]
+    fetch('https://fake-api-tau.vercel.app/api/eplay/esportes')
+      .then((res) => res.json())
+      .then((res) => setGamesEsportes(res))
 
-const Categories = () => (
-  <>
-    <ProductsList background="gray" title="RPG" games={promocoes} />
-    <ProductsList background="black" title="Ação" games={emBreve} />
-    <ProductsList background="gray" title="Aventura" games={promocoes} />
-    <ProductsList background="black" title="FPS" games={emBreve} />
-  </>
-)
+    fetch('https://fake-api-tau.vercel.app/api/eplay/simulacao')
+      .then((res) => res.json())
+      .then((res) => setGamesSimulacao(res))
 
+    fetch('https://fake-api-tau.vercel.app/api/eplay/luta')
+      .then((res) => res.json())
+      .then((res) => setGamesLuta(res))
+
+    fetch('https://fake-api-tau.vercel.app/api/eplay/rpg')
+      .then((res) => res.json())
+      .then((res) => setGamesRPG(res))
+  }, [])
+
+  return (
+    <>
+      <ProductsList background="black" title="Ação" games={gamesAcao} />
+      <ProductsList background="gray" title="Esportes" games={gamesEsportes} />
+      <ProductsList background="black" title="Luta" games={gamesLuta} />
+      <ProductsList background="gray" title="RPG" games={gamesRPG} />
+      <ProductsList
+        background="black"
+        title="Simulação"
+        games={gamesSimulacao}
+      />
+    </>
+  )
+}
 export default Categories
--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,106 +1,58 @@
+import { useEffect, useState } from 'react'
+
 import Banner from '../../components/Banner'
 import ProductsList from '../../components/ProductsList'
-import Game from '../../models/Game'
 
-import resident from '../../assets/img/resident.png'
-import fifa from '../../assets/img/fifa.png'
-import diablo4 from '../../assets/img/diablo.png'
-import starWars from '../../assets/img/star_wars.png'
-import streetFighter from '../../assets/img/street-fighter.png'
-import zelda from '../../assets/img/zelda.png'
+export interface GalleryItem {
+  type: 'image' | 'video'
+  url: string
+}
 
-const promocoes: Game[] = [
-  {
-    id: 1,
-    category: 'Ação',
-    description:
-      'Resident Evil 4, conhecido no Japão como Biohazard 4, é um jogo eletrônico de survival horror...',
-    image: resident,
-    infos: ['10%', 'R$ 199,90'],
-    system: 'Windows',
-    title: 'Resident Evil 4 - Remake'
-  },
-  {
-    id: 2,
-    category: 'Ação',
-    description:
-      'Resident Evil 4, conhecido no Japão como Biohazard 4, é um jogo eletrônico de survival horror...',
-    image: resident,
-    infos: ['10%', 'R$ 199,90'],
-    system: 'Windows',
-    title: 'Resident Evil 4 - Remake'
-  },
-  {
-    id: 3,
-    category: 'Esporte',
-    description:
-      'EA SPORTS™ FIFA 23 traz o Jogo de Todo Mundo aos gramados com a tecnologia HyperMotion2...',
-    image: fifa,
-    infos: ['-50%', 'R$ 99,90'],
-    system: 'PS5',
-    title: 'FIFA 23'
-  },
-  {
-    id: 4,
-    category: 'Esporte',
-    description:
-      'EA SPORTS™ FIFA 23 traz o Jogo de Todo Mundo aos gramados com a tecnologia HyperMotion2...',
-    image: fifa,
-    infos: ['-50%', 'R$ 99,90'],
-    system: 'PS5',
-    title: 'FIFA 23'
+export type Game = {
+  id: number
+  name: string
+  description: string
+  release_date?: string
+  prices: {
+    discount?: number
+    old?: number
+    current?: number
   }
-]
-
-const emBreve: Game[] = [
-  {
-    id: 5,
-    category: 'RPG',
-    description:
-      'Diablo IV é um RPG de ação em desenvolvimento pela Blizzard Entertainment.',
-    image: diablo4,
-    infos: ['05/04'],
-    system: 'Windows',
-    title: 'Diablo 4'
-  },
-  {
-    id: 6,
-    category: 'Aventura',
-    description:
-      'Star Wars Jedi: Survivor é um próximo jogo de ação e aventura desenvolvido pela Respawn...',
-    image: starWars,
-    infos: ['05/04'],
-    system: 'Windows',
-    title: 'Star Wars Jedi Survivor'
-  },
-  {
-    id: 7,
-    category: 'Esporte',
-    description:
-      'Street Fighter 6 é um próximo jogo de luta desenvolvido e publicado pela Capcom.',
-    image: streetFighter,
-    infos: ['05/04'],
-    system: 'Windows',
-    title: 'Street Fighter 6'
-  },
-  {
-    id: 8,
-    category: 'RPG',
-    description:
-      'Uma aventura épica pela terra e pelos céus de Hyrule aguarda em The Legend of Zelda™...',
-    image: zelda,
-    infos: ['05/04'],
-    system: 'Switch',
-    title: 'The Legend of Zelda - TOK'
+  details: {
+    category: string
+    system: string
+    developer: string
+    publisher: string
+    languages: string[]
   }
-]
+  media: {
+    thumbnail: string
+    cover: string
+    gallery: GalleryItem[]
+  }
+}
 
-const Home = () => (
-  <>
-    <Banner />
-    <ProductsList background="gray" title="Promoções" games={promocoes} />
-    <ProductsList background="black" title="Em breve" games={emBreve} />
-  </>
-)
+const Home = () => {
+  const [promocoes, setPromocoes] = useState<Game[]>([])
+  const [emBreve, setEmBreve] = useState<Game[]>([])
+
+  useEffect(() => {
+    fetch('https://fake-api-tau.vercel.app/api/eplay/promocoes')
+      .then((res) => res.json())
+      .then((res) => setPromocoes(res))
+
+    fetch('https://fake-api-tau.vercel.app/api/eplay/em-breve')
+      .then((res) => res.json())
+      .then((res) => setEmBreve(res))
+  }, [])
+
+  return (
+    <>
+      <Banner />
+      <ProductsList background="gray" title="Promoções" games={promocoes} />
+      <ProductsList background="black" title="Em breve" games={emBreve} />
+    </>
+  )
+}
 
 export default Home
